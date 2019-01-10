b8c53d22459d9589223e995dfa4540ab0a40950e
Travis Redman <travis.redman@gmail.com>
2019-01-10 10:08:33 -0800

    remove unused new_event function in internals module

--- beeline/internal.py
+++ beeline/internal.py
@@ -2,12 +2,6 @@ import beeline
 
 # these are mostly convenience methods for safely calling beeline methods
 # even if the beeline hasn't been initialized
-def new_event(data=None, trace_name='', top_level=False):
-    bl = beeline.get_beeline()
-    if bl:
-        bl.log("creating new event with data = %s", data)
-        return bl.new_event(data, trace_name, top_level)
-
 def send_event():
     bl = beeline.get_beeline()
     if bl:
