5c099dcb8eae3d8c350ed127d8ec9f551b7d4925
Douglas Soo <dsoo@alum.mit.edu>
2020-08-27 16:41:21 -0700

    More fixes

--- beeline/propagation/test_w3c.py
+++ beeline/propagation/test_w3c.py
@@ -24,19 +24,21 @@ class TestW3CMarshalUnmarshal(unittest.TestCase):
 
         pc = PropagationContext(_TEST_TRACE_ID, _TEST_PARENT_ID, trace_fields)
 
-        _TEST_TRACEPARENT_HEADER = w3c.marshal_traceparent(pc)
-        self.assertEquals(_TEST_TRACEPARENT_HEADER, _TEST_TRACEPARENT_HEADER)
+        traceparent_header = w3c.marshal_traceparent(pc)
+        tracestate_header = w3c.marshal_tracestate(pc)
 
-        _TEST_TRACESTATE_HEADER = w3c.marshal_tracestate(pc)
-        self.assertEquals(_TEST_TRACESTATE_HEADER, _TEST_TRACESTATE_HEADER)
+        # Make sure marshalled headers are as we expect.
+        self.assertEquals(_TEST_TRACEPARENT_HEADER, traceparent_header)
+        self.assertEquals(_TEST_TRACESTATE_HEADER, tracestate_header)
 
         new_trace_id, new_parent_id, new_trace_flags = w3c.unmarshal_traceparent(
-            _TEST_TRACEPARENT_HEADER)
+            traceparent_header)
+        new_tracestate = w3c.unmarshal_tracestate(tracestate_header)
+
+        # Check round-trip values are the same as start values.
         self.assertEquals(_TEST_TRACE_ID, new_trace_id)
         self.assertEquals(_TEST_PARENT_ID, new_parent_id)
         self.assertEquals(_TEST_TRACE_FLAGS, new_trace_flags)
-
-        new_tracestate = w3c.unmarshal_tracestate(_TEST_TRACESTATE_HEADER)
         self.assertEquals(_TEST_TRACESTATE, new_tracestate)
 
 
