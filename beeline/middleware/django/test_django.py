53395776cf62852add48fcfa7273faf93b53cbcb
Sjoerd Job Postmus <sjoerdjob@sjec.nl>
2020-10-29 22:25:37 +0100

    Disable specific pylint check.

--- beeline/middleware/django/test_django.py
+++ beeline/middleware/django/test_django.py
@@ -39,8 +39,8 @@ class FullViewTestCase(unittest.TestCase):
         # top-level import, the test discovery procedure checks if `settings`
         # is a subclass of `TestCase`, causing the settings to be initialized,
         # which isn't possible.
-        from django.conf import settings
-        from django.utils.functional import empty
+        from django.conf import settings  # pylint: disable=import-outside-toplevel
+        from django.utils.functional import empty  # pylint: disable=import-outside-toplevel
         assert not settings.configured
         # On shutdown:
         self.addCleanup(lambda: setattr(settings, "_wrapped", empty))
