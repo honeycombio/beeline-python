73f5ba443ad19ba6ecb7761f11c64f1652d4df2f
JamieDanielson <jamieedanielson@gmail.com>
2023-05-25 14:21:11 -0400

    make examples more consistent

--- examples/django/app/hello/apps.py
+++ examples/django/app/hello/apps.py
@@ -8,8 +8,11 @@ class HelloConfig(AppConfig):
 
     def ready(self):
         beeline.init(
+            # Get this via https://ui.honeycomb.io/account after signing up for Honeycomb
             writekey=os.environ.get("HONEYCOMB_API_KEY"),
-            api_host="https://api.honeycomb.io",
-            service_name='my-django-app',
-            debug=True,
+            api_host=os.environ.get('HONEYCOMB_API_ENDPOINT', 'https://api.honeycomb.io:443'),
+            # The name of your app is a good choice to start with
+            # dataset='my-django-app', # only needed for classic
+            service_name=os.environ.get('SERVICE_NAME', 'my-django-app'),
+            debug=True, # enable to see telemetry in console
         )
--- examples/flask/app.py
+++ examples/flask/app.py
@@ -4,9 +4,17 @@ import os
 from beeline.middleware.flask import HoneyMiddleware
 from flask import Flask
 
+# Get this via https://ui.honeycomb.io/account after signing up for Honeycomb
 honeycomb_writekey=os.environ.get("HONEYCOMB_API_KEY")
 
-beeline.init(writekey=honeycomb_writekey, dataset="my-flask-app", service_name="my-flask-app-service")
+beeline.init(
+    writekey=honeycomb_writekey,
+    api_host=os.environ.get('HONEYCOMB_API_ENDPOINT', 'https://api.honeycomb.io:443'),
+    # The name of your app is a good choice to start with
+    # dataset='my-flask-app', # only needed for classic
+    service_name=os.environ.get('SERVICE_NAME', 'my-flask-app'),
+    debug=True, # enable to see telemetry in console
+)
 
 # Pass your Flask app to HoneyMiddleware
 app = Flask(__name__)
